e2226f27416c4f3cd9c2432a6d4b62814e0ec33c
CRISTHIAN <criacamu@gmail.com>
2019-07-30 10:11:30 -0500

    se avanza en BD creacion de onjetos

---
+++ app/src/main/java/com/nitrocanar/fundacionhuellas/modelo/Donante.java
@@ -0,0 +1,80 @@
+package com.nitrocanar.fundacionhuellas.modelo;
+
+public class Donante {
+    private int donId;
+    private String donNombre;
+    private String donApellido;
+    private String donEmail;
+    private String donTelefono;
+    private String donContrasenia;
+    private String donTipo;
+
+    public Donante() {
+    }
+
+    public Donante(int donId, String donNombre, String donApellido, String donEmail, String donTelefono, String donContrasenia, String donTipo) {
+        this.donId = donId;
+        this.donNombre = donNombre;
+        this.donApellido = donApellido;
+        this.donEmail = donEmail;
+        this.donTelefono = donTelefono;
+        this.donContrasenia = donContrasenia;
+        this.donTipo = donTipo;
+    }
+
+    public int getDonId() {
+        return donId;
+    }
+
+    public void setDonId(int donId) {
+        this.donId = donId;
+    }
+
+    public String getDonNombre() {
+        return donNombre;
+    }
+
+    public void setDonNombre(String donNombre) {
+        this.donNombre = donNombre;
+    }
+
+    public String getDonApellido() {
+        return donApellido;
+    }
+
+    public void setDonApellido(String donApellido) {
+        this.donApellido = donApellido;
+    }
+
+    public String getDonEmail() {
+        return donEmail;
+    }
+
+    public void setDonEmail(String donEmail) {
+        this.donEmail = donEmail;
+    }
+
+    public String getDonTelefono() {
+        return donTelefono;
+    }
+
+    public void setDonTelefono(String donTelefono) {
+        this.donTelefono = donTelefono;
+    }
+
+    public String getDonContrasenia() {
+        return donContrasenia;
+    }
+
+    public void setDonContrasenia(String donContrasenia) {
+        this.donContrasenia = donContrasenia;
+    }
+
+    public String getDonTipo() {
+        return donTipo;
+    }
+
+    public void setDonTipo(String donTipo) {
+        this.donTipo = donTipo;
+    }
+}
--- app/src/main/java/com/nitrocanar/fundacionhuellas/modelo/ManagerSQLiteHelper.java
+++ app/src/main/java/com/nitrocanar/fundacionhuellas/modelo/ManagerSQLiteHelper.java
@@ -1,4 +1,14 @@
 package com.nitrocanar.fundacionhuellas.modelo;
 
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
 public class ManagerSQLiteHelper {
+    //REFERENCIAS
+    ArrayList<Ninios> lista;
+    Ninios ninios;
+    ConexionSQLiteHelper conexionSQLiteHelper;
+    SQLiteDatabase db;
+
 }
