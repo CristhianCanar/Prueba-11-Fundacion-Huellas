fa346f8a5d3810c7f2ca588d2b4e69bb59f87f90
CRISTHIAN <criacamu@gmail.com>
2019-07-30 09:31:35 -0500

    Se mueve y se cre mvc

--- app/src/androidTest/java/com/nitrocanar/fundacionhuellas/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nitrocanar/fundacionhuellas/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.nitrocanar.fundacionhuellas;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- app/src/main/java/com/nitrocanar/fundacionhuellas/controlador/MainActivity.java
+++ app/src/main/java/com/nitrocanar/fundacionhuellas/controlador/MainActivity.java
@@ -1,7 +1,10 @@
-package com.nitrocanar.fundacionhuellas;
+package com.nitrocanar.fundacionhuellas.controlador;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.nitrocanar.fundacionhuellas.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
